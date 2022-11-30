1db7a3ace3e0c2361232b760ad3eccdd31ed496d
ghamdouni001 <ghofrane_hamdouni@DESKTOP-G4MN9G7.localdomain>
2022-11-30 22:15:31 +0100

    Add Robustness and nominal tests

--- MiniTestProject/PPCM.java
+++ MiniTestProject/PPCM.java
@@ -1,7 +1,10 @@
 package MiniTestProject;
 
 class PPCM{
-    public static int ppcm(int a, int b){
+	//PPCM_HLR : This function shall take two integers.
+    public int ppcm(int a, int b){
+    	//PPCM_LLR : This function shall take two positive integers.
+    	if (a <0  || b <0) return -1;
         int ppcm = 0;
         int i = 1;
         while(ppcm == 0){
--- MiniTestProject/PPCMTest.java
+++ MiniTestProject/PPCMTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 
 class PPCMTest {
 
+	//Nominal
 	@Test
-	void testPPCMPrimaryNumbers() {
-		// Arrange
+	void testToPass() {
 		int a = 2;
 		int b = 3;
 		PPCM calculator = new PPCM();
-		// Act
 		int result = calculator.ppcm(a, b);
-
-		// Assert
 		assertEquals(6, result);
+	}
+	
+	//Robustness
+	@Test
+	void testToFail() {
+		int a = -2;
+		int b = -3;
+		PPCM calculator = new PPCM();
+		int result = calculator.ppcm(a, b);
+		assertEquals(-1, result);
 	}
 
 }
